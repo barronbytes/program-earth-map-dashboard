be0aaeafcda2f14a6a1ae2c11d15750ae65b14a1
vnewto <181774283+vnewto@users.noreply.github.com>
2025-07-09 20:10:33 -0400

    Linked stylesheets for Font Awesome 5 and Material Design Icons in index.html file. Added images folder and uploaded icon images for the Areas section of the Legend. Linked css file in main.js. Built out App.vue to add the four following sections: Map Visualization, Map Information, Nav Menu Bar, and Footer. Map information area includes Maps section for selecting map layers using checkboxes, and Legend section with both text and icons. Set up the app using grid so that the Map Visualization area takes up 8 columns and Map Information takes up 4. Set up media queries so that Map Vis and Map Info stack on top of each other at smaller screen sizes.

--- map-dashboard-ui/src/App.vue
+++ map-dashboard-ui/src/App.vue
@@ -1,12 +1,116 @@
 <template>
   <v-app>
+
+    <!------------ Nav Bar --------------->
+    <v-toolbar>
+      Nav bar goes here
+    </v-toolbar> <!-- /end Nav Bar-->
+
+    <!-- Main Content -->
     <v-main>
-      <v-container class="pa-10">
-        <v-alert type="success" title="Vuetify 3 Installed!">
-          Welcome to the Map Dashboard UI!
-        </v-alert>
-      </v-container>
+
+      <!-- Grid layout -->
+      <v-container>
+        <v-row>
+
+          <!--------- Map Visualization -------->
+          <v-col cols="12" md="8" style="border: 1px solid black;" class="map-vis-col pa-0">
+            <v-sheet>
+              <!-- Test image just as a placeholder -->
+               <v-img
+                  cover
+                  src="/images/Map_Screenshot_Vue_Test.jpg">
+                </v-img>
+            </v-sheet>
+          </v-col> <!-- /end Map Visualization -->
+
+          <!-------------- Map Information -------------->
+          <v-col cols="12" md="4" class="map-info-col">
+
+            <v-sheet class="map-info-reset"> 
+
+              <!-- Title -->
+              <h1 class="text-h1 mb-6">Biodiversity Conservation</h1>
+
+              <!-- Maps section -->
+              <v-container class="pa-0 mb-6">
+                <h2 class="text-h2 mb-4">Maps</h2>
+                <p class="text-body-1">Select to view map layer:</p>
+                <!-- Checkboxes to select map layers -->
+                <v-checkbox label="ProgramEarth Conservation Partners" density="compact" hide-details></v-checkbox>
+                <v-checkbox label="USGS Watershed Boundary Dataset" density="compact" hide-details></v-checkbox>
+                <v-checkbox label="Meta's Canopy Heights" density="compact" hide-details></v-checkbox>                
+              </v-container> <!--/end maps section-->
+              
+              <!-- Legend -->
+              <v-container class="pa-0 mb-6">
+                <h2 class="text-h2 mb-4">Legend</h2>
+
+                <!-- Points -->
+                <div class="points-container">
+                  <h3 class="text-h3 mb-3">Points</h3>
+                  <!-- Landmark -->
+                  <v-container class="points-item mb-3">
+                    <i class="fas fa-circle" style="color: #424242; font-size: 16px;"></i> <p class="text-body-1">Landmark</p> <!-- /end Landmark -->
+                  </v-container>
+                  <!-- Animals -->
+                  <v-container class="points-item mb-3">
+                    <i class="fas fa-dove" style="color: #2196F3; font-size: 16px"></i> <p class="text-body-1">Animals</p>
+                  </v-container> <!-- /end Animals -->
+                  <!-- Plants -->
+                  <v-container class="points-item">
+                    <i class="fas fa-seedling" style="color: #43A047; font-size: 16px;"></i> <p class="text-body-1">Plants</p> <!-- /end plants -->
+                  </v-container> 
+                </div> <!-- /end points -->
+
+                <!-- Areas -->
+                <div class="areas-container">
+                  <h3 class="text-h3 mb-3">Areas</h3> 
+                  <!-- Species -->
+                  <v-container class="areas-item mb-3" style="width: fit-content;">
+                    <v-img
+                      :width="30"
+                      :height="30"
+                      src="/images/species_icon.svg"
+                    ></v-img>
+                    <p class="text-body-1">Species</p>
+                  </v-container> <!-- /end species -->
+                  <!-- Water -->
+                  <v-container class="areas-item mb-3" style="width: fit-content;">
+                    <v-img
+                      :width="30"
+                      :height="30"
+                      src="/images/water_polygon_icon.svg"
+                    ></v-img>
+                    <p class="text-body-1">Water</p>
+                  </v-container> <!-- /end water -->
+                  <!-- Canopy -->
+                  <v-container class="areas-item" style="width: fit-content;">
+                    <v-img
+                      :width="30"
+                      :height="30"
+                      src="/images/canopy_polygon_icon.svg"
+                    ></v-img>
+                    <p class="text-body-1">Canopy</p>
+                  </v-container> <!-- /end canopy -->
+                </div> <!-- /end Areas -->
+                
+              </v-container> <!-- /end Legend -->
+
+            </v-sheet> 
+
+          </v-col> <!-- /end Map Information -->
+
+        </v-row>
+      </v-container> <!-- /end grid layout -->
+
     </v-main>
+
+    <!---------------- Footer ------------------->
+    <v-footer>
+      Footer goes here
+    </v-footer>
+
   </v-app>
 </template>
 
