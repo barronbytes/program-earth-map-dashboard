c031fc10c888074755c5c290eec895a411ff0833
Bryan Lopez <bmlopez024@gmail.com>
2025-08-19 15:27:46 -0700

    organize the files and folders to create a clear project structure

---
+++ frontend/src/main.js
@@ -0,0 +1,21 @@
+// main.js
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// Vuetify core
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+// Create Vuetify instance
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+createApp(App).use(vuetify).mount('#app')
+// Register global components, plugins, etc. here if needed
+
+//import css style sheet
+import './style.css'
